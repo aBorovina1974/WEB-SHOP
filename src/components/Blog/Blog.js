c06c2917651a2178d3cef781c868389697e7ec64
aBorovina1974 <aborovina74@gmail.com>
2023-03-27 22:22:58 +0200

    fix Blog, replacing local json with Firebase

--- src/components/Blog/Blog.js
+++ src/components/Blog/Blog.js
@@ -1,15 +1,39 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import styles from "./Blog.module.scss";
 import BlogItem from "./BlogItem/BlogItem";
-import Blogs from "../../data/blogs.json";
 
 const Blog = () => {
+  const [blog, setBlog] = useState([]);
+
+  useEffect(() => {
+    const fetchItems = async () => {
+      const response = await fetch(
+        "https://web-shop-7d1b6-default-rtdb.firebaseio.com/blog.json"
+      );
+      const responseData = await response.json();
+      console.log(responseData);
+      const loadedItems = [];
+      for (const key in responseData) {
+        loadedItems.push({
+          id: key,
+          type: responseData[key].type,
+          title: responseData[key].title,
+          text: responseData[key].text,
+          created: responseData[key].created,
+          author: responseData[key].author,
+        });
+      }
+      setBlog(loadedItems);
+    };
+    fetchItems();
+  }, []);
+
   return (
     <section className={styles.section}>
       <h1 className={styles.title}>Blog</h1>
       <ul className={styles.items}>
-        {Blogs &&
-          Blogs.map((blogItem) => (
+        {blog &&
+          blog.map((blogItem) => (
             <li key={blogItem.id} className={styles.item}>
               <BlogItem
                 blog={{
