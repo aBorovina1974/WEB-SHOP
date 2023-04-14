df764bd0d8817c465c548dba05caa4e9074335ef
aBorovina1974 <aborovina74@gmail.com>
2023-04-14 22:27:53 +0200

    fixing context

--- src/contexts/cart/CartContextProvider.js
+++ src/contexts/cart/CartContextProvider.js
@@ -1,4 +1,4 @@
-import {createContext, useReducer} from "react";
+import { createContext, useReducer } from "react";
 
 const CartContext = createContext(null);
 const initialCart = [];
@@ -6,36 +6,75 @@ const initialCart = [];
 const reducer = (state = initialCart, action) => {
   switch (action.type) {
     case "update":
-      const updatedIndex = state.findIndex((item) => item.id === action.payload.id);
+      const updatedIndex = state.findIndex(
+        (item) => item.id === action.payload.id
+      );
       if (updatedIndex === -1) {
         return [...state, action.payload];
       } else {
         const newState = [...state];
         newState[updatedIndex] = {
           ...newState[updatedIndex],
-          ...action.payload
+          ...action.payload,
         };
         return newState;
       }
-    case "reset":
+    case "remove":
+      const itemIndex = state.findIndex(
+        (item) => item.id === action.payload.id
+      );
+      if (itemIndex !== -1) {
+        const newState = [...state];
+        newState.splice(itemIndex, 1);
+        return newState;
+      } else {
+        return state;
+      }
+    case "clear":
       return initialCart;
     default:
       throw new Error(`Unknown action type: ${action.type}`);
   }
 };
 
-const CartProvider = ({children}) => {
+const CartProvider = ({ children }) => {
   const [state, dispatch] = useReducer(reducer);
 
   const updateCart = (product) => {
-    dispatch({type: "update", payload: product});
+    dispatch({ type: "update", payload: product });
+  };
+
+  const removeFromCart = (product) => {
+    dispatch({ type: "remove", payload: product });
+  };
+
+  const clearCart = () => {
+    dispatch({ type: "clear" });
+  };
+
+  const calculateTotal = () => {
+    let total = 0;
+    if (state) {
+      state.forEach((product) => {
+        total += product.price * product.quantity;
+      });
+    }
+    return total;
   };
 
   return (
-    <CartContext.Provider value={{cart: state, updateCart}}>
+    <CartContext.Provider
+      value={{
+        cart: state,
+        updateCart,
+        removeFromCart,
+        clearCart,
+        calculateTotal,
+      }}
+    >
       {children}
     </CartContext.Provider>
   );
 };
 
-export {CartContext, CartProvider};
+export { CartContext, CartProvider };
